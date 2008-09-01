4dd8a68c7de4043343c5d4b7ee5e3613b336ed05
Stephen McKamey <mckamey@users.noreply.github.com>
2008-09-01 00:08:21 +0000

    - removing hardcoded MIME constants

--- External/MimeTypes.cs
+++ External/MimeTypes.cs
@@ -50,14 +50,6 @@ namespace MimeUtils
 		private static readonly Dictionary<string, MimeType> MimeByExtension;
 		private static readonly Dictionary<string, MimeType> MimeByContentType;
 		public static readonly MimeType[] ConfigTypes;
-		public static readonly MimeType ImageJpeg;
-		public static readonly MimeType ImageGif;
-		public static readonly MimeType ImagePng;
-		public static readonly MimeType JavaScript;
-		public static readonly MimeType CssStyleSheet;
-		public static readonly MimeType Html;
-		public static readonly MimeType Xml;
-		public static readonly MimeType Rss;
 
 		internal const string JavaScriptContentType = "application/javascript";
 		internal const string CssStyleSheetContentType = "text/css";
@@ -144,112 +136,6 @@ namespace MimeUtils
 				MimeTypes.MimeByExtension = new Dictionary<string, MimeType>(MimeTypes.ConfigTypes.Length);
 				MimeTypes.MimeByContentType = new Dictionary<string, MimeType>(MimeTypes.ConfigTypes.Length);
 			}
-			finally
-			{
-				MimeTypes.ImageJpeg = MimeTypes.GetByExtension(".jpg");
-				if (MimeTypes.ImageJpeg == null)
-				{
-					MimeTypes.ImageJpeg = new MimeType();
-					MimeTypes.ImageJpeg.Name = "JPEG Image";
-					MimeTypes.ImageJpeg.FileExts = new String[] { ".jpg", ".jpeg" };
-					MimeTypes.ImageJpeg.ContentTypes = new String[] { "image/jpeg", "image/pjpeg" };
-					MimeTypes.ImageJpeg.Category = MimeCategory.Image;
-					MimeTypes.ImageJpeg.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.ImageJpeg.FileExts[0]] = MimeTypes.ImageJpeg;
-					MimeTypes.MimeByContentType[MimeTypes.ImageJpeg.ContentTypes[0]] = MimeTypes.ImageJpeg;
-				}
-
-				MimeTypes.ImageGif = MimeTypes.GetByExtension(".gif");
-				if (MimeTypes.ImageGif == null)
-				{
-					MimeTypes.ImageGif = new MimeType();
-					MimeTypes.ImageGif.Name = "GIF Image";
-					MimeTypes.ImageGif.FileExts = new String[] { ".gif", ".gif" };
-					MimeTypes.ImageGif.ContentTypes = new String[] { "image/gif" };
-					MimeTypes.ImageGif.Category = MimeCategory.Image;
-					MimeTypes.ImageGif.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.ImageGif.FileExts[0]] = MimeTypes.ImageGif;
-					MimeTypes.MimeByContentType[MimeTypes.ImageGif.ContentTypes[0]] = MimeTypes.ImageGif;
-				}
-
-				MimeTypes.ImagePng = MimeTypes.GetByExtension(".png");
-				if (MimeTypes.ImagePng == null)
-				{
-					MimeTypes.ImagePng = new MimeType();
-					MimeTypes.ImagePng.Name = "PNG Image";
-					MimeTypes.ImagePng.FileExts = new String[] { ".png" };
-					MimeTypes.ImagePng.ContentTypes = new String[] { "image/png" };
-					MimeTypes.ImagePng.Category = MimeCategory.Image;
-					MimeTypes.ImagePng.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.ImagePng.FileExts[0]] = MimeTypes.ImagePng;
-					MimeTypes.MimeByContentType[MimeTypes.ImagePng.ContentTypes[0]] = MimeTypes.ImagePng;
-				}
-
-				MimeTypes.JavaScript = MimeTypes.GetByExtension(".js");
-				if (MimeTypes.JavaScript == null)
-				{
-					MimeTypes.JavaScript = new MimeType();
-					MimeTypes.JavaScript.Name = "JavaScript";
-					MimeTypes.JavaScript.FileExts = new String[] { ".js" };
-					MimeTypes.JavaScript.ContentTypes = new String[] { MimeTypes.JavaScriptContentType, "application/ecmascript", "text/javascript", "text/ecmascript" };
-					MimeTypes.JavaScript.Category = MimeCategory.Code;
-					MimeTypes.JavaScript.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.JavaScript.FileExts[0]] = MimeTypes.JavaScript;
-					MimeTypes.MimeByContentType[MimeTypes.JavaScript.ContentTypes[0]] = MimeTypes.JavaScript;
-				}
-
-				MimeTypes.CssStyleSheet = MimeTypes.GetByExtension(".css");
-				if (MimeTypes.CssStyleSheet == null)
-				{
-					MimeTypes.CssStyleSheet = new MimeType();
-					MimeTypes.CssStyleSheet.Name = "Cascading StyleSheet";
-					MimeTypes.CssStyleSheet.FileExts = new String[] { ".css" };
-					MimeTypes.CssStyleSheet.ContentTypes = new String[] { MimeTypes.CssStyleSheetContentType };
-					MimeTypes.CssStyleSheet.Category = MimeCategory.Web;
-					MimeTypes.CssStyleSheet.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.CssStyleSheet.FileExts[0]] = MimeTypes.CssStyleSheet;
-					MimeTypes.MimeByContentType[MimeTypes.CssStyleSheet.ContentTypes[0]] = MimeTypes.CssStyleSheet;
-				}
-
-				MimeTypes.Xml = MimeTypes.GetByExtension(".xml");
-				if (MimeTypes.Xml == null)
-				{
-					MimeTypes.Xml = new MimeType();
-					MimeTypes.Xml.Name = "XML";
-					MimeTypes.Xml.FileExts = new String[] { ".xml" };
-					MimeTypes.Xml.ContentTypes = new String[] { "application/xml", "text/xml" };
-					MimeTypes.Xml.Category = MimeCategory.Xml;
-					MimeTypes.Xml.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.Xml.FileExts[0]] = MimeTypes.Xml;
-					MimeTypes.MimeByContentType[MimeTypes.Xml.ContentTypes[0]] = MimeTypes.Xml;
-				}
-
-				MimeTypes.Rss = MimeTypes.GetByExtension(".rss");
-				if (MimeTypes.Rss == null)
-				{
-					MimeTypes.Rss = new MimeType();
-					MimeTypes.Rss.Name = "RSS";
-					MimeTypes.Rss.FileExts = new String[] { ".rss" };
-					MimeTypes.Rss.ContentTypes = new String[] { "application/rss+xml" };
-					MimeTypes.Rss.Category = MimeCategory.Xml;
-					MimeTypes.Rss.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.Rss.FileExts[0]] = MimeTypes.Rss;
-					MimeTypes.MimeByContentType[MimeTypes.Rss.ContentTypes[0]] = MimeTypes.Rss;
-				}
-
-				MimeTypes.Html = MimeTypes.GetByExtension(".htm");
-				if (MimeTypes.Html == null)
-				{
-					MimeTypes.Html = new MimeType();
-					MimeTypes.Html.Name = "HTML";
-					MimeTypes.Html.FileExts = new String[] { ".htm", ".html", ".xhtm", ".xhtml" };
-					MimeTypes.Html.ContentTypes = new String[] { "text/html", "application/xhtml+xml" };
-					MimeTypes.Html.Category = MimeCategory.Web;
-					MimeTypes.Html.Primary = true;
-					MimeTypes.MimeByExtension[MimeTypes.Html.FileExts[0]] = MimeTypes.Html;
-					MimeTypes.MimeByContentType[MimeTypes.Html.ContentTypes[0]] = MimeTypes.Html;
-				}
-			}
 		}
 
 		#endregion Init
@@ -259,11 +145,15 @@ namespace MimeUtils
 		public static MimeType GetByExtension(string extension)
 		{
 			if (String.IsNullOrEmpty(extension))
+			{
 				return null;
+			}
 
 			extension = extension.ToLowerInvariant();
 			if (!MimeTypes.MimeByExtension.ContainsKey(extension))
+			{
 				return null;
+			}
 
 			return MimeTypes.MimeByExtension[extension];
 		}
@@ -271,11 +161,15 @@ namespace MimeUtils
 		public static MimeType GetByContentType(string contentType)
 		{
 			if (String.IsNullOrEmpty(contentType))
+			{
 				return null;
+			}
 
 			contentType = contentType.ToLowerInvariant();
 			if (!MimeTypes.MimeByContentType.ContainsKey(contentType))
+			{
 				return null;
+			}
 
 			return MimeTypes.MimeByContentType[contentType];
 		}
@@ -292,7 +186,9 @@ namespace MimeUtils
 		{
 			MimeType mime = MimeTypes.GetByExtension(extension);
 			if (mime == null)
+			{
 				return String.Empty;
+			}
 
 			return mime.ContentType;
 		}
@@ -300,11 +196,15 @@ namespace MimeUtils
 		public static MimeCategory GetCategory(string extension)
 		{
 			if (String.IsNullOrEmpty(extension))
+			{
 				return MimeCategory.Folder;
+			}
 
 			MimeType mime = MimeTypes.GetByExtension(extension);
 			if (mime == null)
+			{
 				return MimeCategory.Unknown;
+			}
 
 			return mime.Category;
 		}
@@ -312,14 +212,20 @@ namespace MimeUtils
 		public static System.Drawing.Imaging.ImageFormat GetImageFormat(string extension)
 		{
 			if (String.IsNullOrEmpty(extension))
+			{
 				return null;
+			}
 
 			// use MimeTypes collection to standardize the extension
 			MimeType mime = MimeTypes.GetByExtension(extension);
 			if (mime != null)
+			{
 				extension = mime.FileExt.ToLowerInvariant();
+			}
 			else
+			{
 				extension = extension.ToLowerInvariant();
+			}
 
 			switch (extension.TrimStart('.'))
 			{
