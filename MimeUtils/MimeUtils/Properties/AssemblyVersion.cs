433015bee0ed7b8abf464f11f46941f82ad43c94
Stephen McKamey <mckamey@users.noreply.github.com>
2009-03-10 14:42:10 +0000

    - fixing logic for loading outside of web apps - adding flash video MIME

--- MimeUtils/MimeUtils/Properties/AssemblyVersion.cs
+++ MimeUtils/MimeUtils/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.902.1710
+#region 1.0.903.1007
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.902.1710")]
+[assembly: AssemblyVersion("1.0.903.1007")]
 
-#endregion 1.0.902.1710
+#endregion 1.0.903.1007
