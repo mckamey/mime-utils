76662db4d2958563ee8ccb861dc6e888838cfaf0
smm <smm@6fb59786-6f31-11de-90c3-cb42dc00f2a0>
2009-12-16 06:01:08 +0000

    - allowing methods that take extensions to accept with or without leading dot - added methods to get image encoder/decoder from MimeType

--- trunk/MimeUtils/MimeUtils/Properties/AssemblyVersion.cs
+++ trunk/MimeUtils/MimeUtils/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.909.1722
+#region 1.0.912.1521
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.909.1722")]
-[assembly: AssemblyFileVersion("1.0.909.1722")]
+[assembly: AssemblyVersion("1.0.912.1521")]
+[assembly: AssemblyFileVersion("1.0.912.1521")]
 
-#endregion 1.0.909.1722
+#endregion 1.0.912.1521
